ef37be93a6c3401a82931099ac2e5a56c21744f9
SamSepi0llll <david.bonardi@hotmail.com>
2024-07-23 07:19:44 -0300

    Type problem

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,17 @@
+import Title from '../../components/Title'
+import { P as Paragraph } from '../../components/Paragraph/styles'
+
+const About = () => {
+  return (
+    <section>
+      <Title fontSize={16}>Sobre mim</Title>
+      <Paragraph tipo="principal">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magni
+        eum nisi enim sequi repellat repudiandae facere, aspernatur consequuntur
+        eos hic aperiam facilis aut maiores in! Eveniet pariatur fuga ad.
+      </Paragraph>
+    </section>
+  )
+}
+
+export default About
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <img src="https://github.com/samsepi0llll.png" />
+      <Title fontSize={20}>David Bonardi</Title>
+    </aside>
+  )
+}
+
+export default Sidebar
